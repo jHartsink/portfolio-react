444a1fbd475a09a4e22932adde2fa72b59ce7f30
JHartsink <97026009+jHartsink@users.noreply.github.com>
2022-03-02 15:19:01 +0100

    added gsap
    added gsap to footer & nav

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../../styles/footer-styles.css";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaMailBulk } from "react-icons/fa";
+import gsap from "gsap";
+
 
 export default function Footer() {
+  
+  const footer = useRef();
+
+useEffect(() => {
+  if (footer.current) {
+    gsap.to(footer.current, {
+      delay: 1,
+      opacity: 1,
+      ease: "power2.inOut",
+    });
+  }
+});
+
   return (
     <>
-      <div className="footer-wrapper">
+      <div ref={footer} className="footer-wrapper">
         <div className="button-styles">
           <a
             href="https://www.linkedin.com/in/justin-hartsink-69184b16/"
--- src/components/navbar/navigation-top.js
+++ src/components/navbar/navigation-top.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import "../../styles/style-navbar.css";
 import { NavLink } from "react-router-dom";
+import gsap from "gsap";
 
 export default function NavBar() {
   const [click, setClick] = useState(false);
   const Close = () => setClick(false);
   const handleClick = () => setClick(!click);
+  const navbar = useRef();
+
+  useEffect(() => {
+    if (navbar.current) {
+      gsap.to(navbar.current, {
+        delay: 1,
+        opacity: 1,
+        ease: "power2.inOut",
+      });
+    }
+  });
 
   return (
     <>
       <div className={click ? "main-container" : ""} onClick={() => Close()} />
-      <nav className="navbar">
+      <nav ref={navbar} className="navbar">
         <div className="nav-container">
           <NavLink to="/" className="nav-logo">
             Justin.Hartsink
